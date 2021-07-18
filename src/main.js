15ca9f43c99022bc7a715f0db8f1b9fa54c9808d
PedroHenrique-git <pedromf34@outlook.com>
2021-07-18 16:16:05 -0300

    ajustes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import './assets/css/style.css';
-import formulario from './modules/formularios';
 import iniciar from './modules/formularios';
 
 (function(){
--- src/modules/formularios.js
+++ src/modules/formularios.js
@@ -1,25 +1,25 @@
 import gerarSenha from './geradores';
 
-let p = document.querySelector('p');
+const p = document.querySelector('p');
 
-let inputResultado = document.querySelector('#res');
-let inputRange = document.querySelector('input[type=number]');
-let inputNumber = document.querySelector('#addNumber');
-let inputMaiuscula = document.querySelector('#addMaiusculas');
-let inputMinuscula = document.querySelector('#addMinusculas');
-let inputSimbolos = document.querySelector('#addSimbolos');
-let button = document.querySelector('button');
+const inputResultado = document.querySelector('#res');
+const inputRange = document.querySelector('input[type=number]');
+const inputNumber = document.querySelector('#addNumber');
+const inputMaiuscula = document.querySelector('#addMaiusculas');
+const inputMinuscula = document.querySelector('#addMinusculas');
+const inputSimbolos = document.querySelector('#addSimbolos');
+const button = document.querySelector('button');
 
 const iniciar = () =>
 {
         button.addEventListener('click',() =>{
-        inputResultado.value = gerarSenha(
-            inputRange.value,
-            inputNumber.checked,
-            inputMaiuscula.checked,
-            inputMinuscula.checked,
-            inputSimbolos.checked   
-        )    
+            inputResultado.value = gerarSenha(
+                inputRange.value,
+                inputNumber.checked,
+                inputMaiuscula.checked,
+                inputMinuscula.checked,
+                inputSimbolos.checked   
+            )    
         });
 }
 
